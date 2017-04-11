6c969640fb2dd2a533fdfaa75855865b7b3df584
ravisalagame <ravirs@yahoo.com>
2017-04-11 15:11:32 +0530

    Add files via upload

---
+++ server.R
@@ -0,0 +1,73 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(datasets)
+library(ggplot2)
+
+# We tweak the "am" field to have nicer factor labels. Since this doesn't
+# rely on any user inputs we can do this once at startup and then use the
+# value throughout the lifetime of the application
+mpgData <- mtcars
+mpgCyl4 <- subset(mpgData,cyl==4)
+mpgCyl6 <- subset(mpgData,cyl==6)
+mpgCyl8 <- subset(mpgData,cyl==8)
+
+# Define server logic required to plot various variables against mpg
+shinyServer(function(input, output) {
+  
+  # Compute the forumla text in a reactive expression since it is 
+  # shared by the output$caption and output$mpgPlot expressions
+  formulaText <- reactive({
+    paste("mpg ~", input$nCyl)
+  })
+  
+  # Return the formula text for printing as a caption
+  output$caption <- renderText({
+    formulaText()
+  })
+
+  test1 <- reactive({     
+    if(input$nCyl=="Cyl4") mpgData<-mpgCyl4
+    if(input$nCyl=="Cyl6") mpgData<-mpgCyl6
+    if(input$nCyl=="Cyl8") mpgData<-mpgCyl8
+    input$nCyl
+  }) 
+  
+  test2 <- reactive({
+    input$plottyp
+  })
+  
+  # Generate a plot of the requested variable against mpg and only 
+  # include outliers if requested
+  output$mpgPlot <- renderPlot({
+    if(test1()=="Cyl4") {
+        if(test2()=="wt") {
+          ggplot(data=mpgCyl4,aes(x=wt,y=mpg))+geom_point()+geom_smooth(method='lm',se=FALSE)
+        } else {
+          ggplot(data=mpgCyl4,aes(x=hp,y=mpg))+geom_point()+geom_smooth(method='lm',se=FALSE)
+        }
+    } else {
+        if(test1()=="Cyl6"){
+          if(test2()=="wt") {
+            ggplot(data=mpgCyl6,aes(x=wt,y=mpg))+geom_point()+geom_smooth(method='lm',se=FALSE)
+          } else {
+            ggplot(data=mpgCyl6,aes(x=hp,y=mpg))+geom_point()+geom_smooth(method='lm',se=FALSE)
+          }
+        } else {
+          if(test2()=="wt") {
+            ggplot(data=mpgCyl8,aes(x=wt,y=mpg))+geom_point()+geom_smooth(method='lm',se=FALSE)
+          } else {
+            ggplot(data=mpgCyl8,aes(x=hp,y=mpg))+geom_point()+geom_smooth(method='lm',se=FALSE)
+          }
+        }
+    }
+
+  })
+})
---
+++ ui.R
@@ -0,0 +1,36 @@
+library(shiny)
+
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("MPg v/s Other Variables from mtcars data"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    selectInput("nCyl", "No. of Cylinders:",
+                list("4" = "Cyl4", 
+                     "6" = "Cyl6", 
+                     "8" = "Cyl8")),
+    
+    selectInput("plottyp", "Plot mpg v/s ?",
+                list("wt" = "wt", 
+                     "hp" = "hp")),
+    
+    helpText("This application will plot mpg v/s another variables for",
+             "selected number of cylinders. You may choose weight,",
+              "displacement or hp as one of the other variables.",
+              "For example, you can plot mpg v/s hp for all 4 cylinder engines"),
+    
+    
+    checkboxInput("outliers", "Show outliers", FALSE)
+  ),
+  
+  # Show the caption and plot of the requested variable against mpg
+  mainPanel(
+    h3(textOutput("caption")),
+    
+    plotOutput("mpgPlot")
+  )
+))
